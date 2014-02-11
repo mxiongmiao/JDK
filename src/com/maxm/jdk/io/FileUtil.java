6293d80da1a939aafdd00ae16836e3ad49cd7a9e
mxiongmiao <mxiongmiao@gmail.com>
2014-02-11 19:11:33 +0800

    add nio exam

--- src/com/maxm/jdk/io/FileUtil.java
+++ src/com/maxm/jdk/io/FileUtil.java
@@ -4,17 +4,20 @@ import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 import com.maxm.jdk.util.PrintUtil;
 
-public class FileRead {
+public class FileUtil {
 	private static final String lineSeperator = System.lineSeparator();
 	private StringBuilder data = new StringBuilder();
 
-	public FileRead() {
+	public FileUtil() {
 		super();
 	}
 
@@ -36,6 +39,25 @@ public class FileRead {
 		close(in);
 	}
 
+	public void copy(File from, File to) throws IOException {
+		// bio 的话直接从 fin 中读取，而nio会从fin的通道中读取
+		FileChannel fci = new FileInputStream(from).getChannel();
+		FileChannel fco = new FileOutputStream(to).getChannel();
+
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		for (;;) {
+			// 从输入通道读取数据到缓冲区
+			buffer.clear();
+			int r = fci.read(buffer);
+			if (r == -1) {
+				break;
+			}
+			// 将当期那缓冲区内容写入输出通道
+			buffer.flip();
+			fco.write(buffer);
+		}
+	}
+
 	private void close(Closeable br) throws IOException {
 		if (br != null) {
 			br.close();
@@ -44,8 +66,11 @@ public class FileRead {
 
 	public static void main(String[] args) throws IOException {
 		File f = new File("E:/快盘/work/rekoo/my脚本/queryUser.sh");
-		FileRead fr = new FileRead();
+		FileUtil fr = new FileUtil();
 		fr.read(f);
 		PrintUtil.println(fr.data);
+
+		File f2 = new File("E:/GitHub/JDK/src/com/maxm/jdk/io/queryUser.sh");
+		fr.copy(f, f2);
 	}
 }
