fae7d42d5f6b021619d6d2f7f5d7a08a1d7ea3aa
mxiongmiao <mxiongmiao@gmail.com>
2014-02-12 17:23:06 +0800

    测试优化重构

--- src/com/maxm/jdk/io/nio/BufferTest.java
+++ src/com/maxm/jdk/io/nio/BufferTest.java
@@ -2,6 +2,7 @@ package com.maxm.jdk.io.nio;
 
 import java.nio.ByteBuffer;
 
+import com.maxm.jdk.util.BufferUtil;
 import com.maxm.jdk.util.PrintUtil;
 
 public class BufferTest {
@@ -9,35 +10,36 @@ public class BufferTest {
 	public static void main(String[] args) {
 		// 构建并填充一缓冲区
 		ByteBuffer buffer = ByteBuffer.allocate(10);
-		String[] bufferState = { "position:" + buffer.position(),
-				"limit:" + buffer.limit(), "capacity:" + buffer.capacity() };
-		PrintUtil.showAsOneLine(bufferState);
-		
+		BufferUtil.showBufferState(buffer);
+
 		for (int i = 0; i < buffer.capacity(); i++) {
 			buffer.put((byte) i);
 		}
+		PrintUtil.println("原缓冲区:");
+		BufferUtil.viewBuffer(buffer);
+
 		// 创建子缓冲区(分片)
+		PrintUtil.println("创建子缓冲区 positon(1,3):");
 		buffer.position(1);
 		buffer.limit(3);
 		ByteBuffer slice = buffer.slice();
-		ByteBuffer oldBuffer = buffer.asReadOnlyBuffer();
-		viewBuffer(oldBuffer);
-		PrintUtil.println(";;;");
+		PrintUtil.println("子缓冲区:");
+		BufferUtil.viewBuffer(slice);
+		// 转换为只读缓冲区
+		// ByteBuffer oldBuffer = buffer.asReadOnlyBuffer();
+		// BufferUtil.viewBuffer(oldBuffer);
+		// 修改子缓冲区内容
+		PrintUtil.println("子缓冲区每个元素*2");
 		for (int i = 0; i < slice.capacity(); i++) {
 			byte b = slice.get(i);
 			b *= 2;
 			slice.put(i, b);
 		}
-		viewBuffer(buffer);
-	}
-
-	private static void viewBuffer(ByteBuffer buffer) {
-		// 查看原来缓冲区内容
-		buffer.position(0);
-		buffer.limit(buffer.capacity());
-		while (buffer.remaining() > 0) {
-			PrintUtil.println(buffer.get());
-		}
+		PrintUtil.println("子缓冲区：");
+		BufferUtil.viewBuffer(slice);
+		PrintUtil.println("原缓冲区：");
+		BufferUtil.viewBuffer(buffer);
+		PrintUtil.println("显然子缓冲区元素的修改映射到了原缓冲区,可见它们共享底层数组");
 	}
 
 }
--- src/com/maxm/jdk/util/BufferUtil.java
+++ src/com/maxm/jdk/util/BufferUtil.java
@@ -1,6 +1,7 @@
 package com.maxm.jdk.util;
 
 import java.nio.Buffer;
+import java.nio.ByteBuffer;
 
 public class BufferUtil {
 
@@ -8,6 +9,18 @@ public class BufferUtil {
 		String[] meta = { "position:" + buffer.position(),
 				"limit:" + buffer.limit(), "capacity:" + buffer.capacity() };
 		PrintUtil.showAsOneLine(meta);
+	}
+
+	// 查看原来缓冲区内容
+	public static void viewBuffer(ByteBuffer buffer) {
+		buffer.position(0);
+		buffer.limit(buffer.capacity());
+		StringBuffer ret = new StringBuffer(buffer.capacity());
+		while (buffer.remaining() > 0) {
+			ret.append(buffer.get()).append(",");
+		}
+		ret.setLength(ret.length() - 1);
+		PrintUtil.println(ret);
 	}
 
 }
